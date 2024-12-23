497c017f696940ecca82f75bb6b846146997e506
Stexinator <hurricane90@gmx.de>
2024-12-23 16:39:37 +0100

    initial commit

---
+++ src/scripts/hooks.js
@@ -0,0 +1,11 @@
+Hooks.once('init', function () {
+    if (game.modules.get('autoanimations')?.active) {
+        game.settings.register('autoanimations', 'criticalAnimation', {
+            name: 'Critical Effect',
+            hint: 'This will play an effect on the token that gets hit by a critical',
+            scope: 'world',
+            config: true,
+            type: String
+        });
+    }
+});
