3bcab0767ec665b403b075102986fd0705a0f98d
Stexinator <hurricane90@gmx.de>
2025-12-27 14:30:24 +0100

    deprecate module

--- src/scripts/hooks.js
+++ src/scripts/hooks.js
@@ -1,12 +1,16 @@
-import AASettings from './autoanimation/aa-setting.js';
 import AutoCritHandling from './combat/autoCrit.js';
 import CombatSettings from './combat/combatSettings.js';
 import InitativeHandling from './combat/deleteIni.js';
 
-Hooks.once('ready', function () {
+Hooks.once('ready', async function () {
     if (game.users.activeGM !== game.user) return;
-    AASettings.addCritAnimation();
     CombatSettings.addCombatSetting();
+
+    foundry.applications.api.DialogV2.prompt({
+        window: { title: 'ImpMal QoL Deprecated' },
+        content:
+            '<p>ImpMal Mini QoL is deprecated. The functionality was added to https://foundryvtt.com/packages/impmal-community</p>'
+    });
 });
 
 Hooks.on('preCreateChatMessage', function (message) {
